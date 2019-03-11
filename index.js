79c3fc56ae43a1e2a02c3ea8a2b7a9356ad987a9
Aleister Montfort <aleistermontfort@Aleisters-MacBook-Pro.local>
2019-03-10 23:26:04 -0500

    map and lines

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ function myVis([data,geodata]) {
   const geoGenerator = d3.geoPath(projection);
   projection.fitExtent([[0, 0], [650, 500]], geodata);
 
-  // X SCALE
+  // X SCALE, years
   const x_scale = d3.scaleBand()
     .domain(data.map((row)=>row.year_death))
     .range([margin.right, plotWidth]);
-  // Y scale is the rate
+  // Y SCALE, rate
   var max_y_domain = d3.max(data, function(d) { return Math.max(d.rate)} );
   const y_scale = d3.scaleLinear()
       .domain([0,max_y_domain]) //
@@ -43,62 +43,196 @@ function myVis([data,geodata]) {
 
   const xAxis = d3.axisBottom()
   .scale(x_scale);
-   //xAxis.tickFormat(d3.timeFormat('%Y'));
-  //xAxis.tickSize(5);
+  const yAxis = d3.axisRight(y_scale);
 
-  const yAxis = d3.axisLeft(y_scale);
-
-  //const states = Object.keys(data.state_name);
-  //const color = d3.scaleOrdinal().domain(states).range(colorRange);
-  //console.log(Object.entries(data.state_name)); // [ ['0', 'a'], ['1', 'b'], ['2', 'c'] ]
 
   var line = d3.line()
-      .x(function(d) { return x_scale(d.year_death); }) // set the x values for the line generator
+      .x(function(d) {
+
+        return x_scale(d.year_death);
+      }) // set the x values for the line generator
       .y(function(d) { return y_scale(d.rate); }) // set the y values for the line generator
       .curve(d3.curveMonotoneX); // apply smoothing to the line
 
-  const svg = d3.select(".main")
+  const svg_chart = d3.select(".main")
         	.append("svg") // why  do we append after we define svg?
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
           .append("g")
           .attr("transform", `translate(${margin.left}, ${margin.top}) `)
 
-          // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-          // 3. Call the x axis in a group tag
   xAxis.tickValues([1998, 2003, 2008, 2013, 2017,]);
-  svg.append("g")
+  svg_chart.append("g")
       .attr("class", "x axis")
       //.attr("transform", "translate(0," + height + ")")
-      .attr("transform", "translate(500, 500)")
+      .attr("transform", "translate(0, 500)")
       .call(xAxis); // Create an axis component with d3.axisBottom
   xAxis.tickSize(0);
   yAxis.tickSize(5);
   yAxis.ticks(5);
   xAxis.ticks(0);
 
-  svg.append("g")
+  svg_chart.append("g")
      .attr("class", "y axis")
-     //.attr("transform", "translate(0," + 100  + ")")
-     .attr("transform", "translate(49,0)")
+     .attr("transform", "translate(500,0)")
      .call(yAxis);
 
-  svg.append("path")
-      .datum(data) // Binds data to the line
-      .attr("class", "line") // Assign a class for styling
-      .attr("d", line(data)) // Calls the line generator
-      .attr("stroke", 'blue');
+  // svg_chart.append("path")
+  //     .datum(data) // Binds data to the line
+  //     .attr("class", "line") // Assign a class for styling
+  //     .attr("d", line(data))
+  //     .attr("fill", "none")// Calls the line generator
+  //     .attr("stroke", '#2171b5');
+  //     .attr("id", "container1");
+
+//var state = svg.createElement("stateid");
+let lines = svg_chart.append("g")
+      .attr("class", "lines");
+
+const lines_data = data.reduce((acc, row) => {
+
+  if (!acc[row.state_name]){
+     acc[row.state_name]=[] ;
+}
+acc[row.state_name].push(row);
+return acc;
+},{});
+console.log(lines_data);
+// lines.selectAll(".state-line")
+//        .data(Object.values(lines_data)).enter()
+//        // .append("g")
+//        // .attr("class", "state-line")
+//        // .attr("state_id",(data) => (data.cve_edo))
+//        .append("path")
+//        .attr("class", "state-line")
+//        .attr("d", d => line((d)))
+//        .attr("stroke", (d, i) => i % 2 ? 'red' : '#2171b5')
+//        .attr("fill", "none");
+//        .on(“mouseout”, function(d) {
+//            svg.select(“.title-text”).remove();
+//          })
 
 
-  svg.selectAll(".state")
+  const svg_map = d3.select(".main")
+          .append("svg")
+          .attr("width", 820)
+          .attr("height", 800)
+          .attr("id", "mapcontainer")
+          .append("g")
+          .attr("transform", `translate(${margin.left}, ${margin.top}) `);
+
+svg_map.selectAll(".state")
           .data(geodata.features)
           .enter()
           .append('path')
             .attr("d", geoGenerator)
+            .attr("id", "container1")
             .attr('class', 'state')
             .attr('stroke', 'white')
-            .attr('fill', "grey");
+            .attr('fill', "#2171b5")
+            .on("click", function(data){
+              console.log(data.properties.CVE_ENT);
+              d3.select(this).attr("fill-opacity",".4");
+              lines.selectAll(".state-line")
+                     .data(Object.values(lines_data)).enter()
+                     // .append("g")
+                     //.attr("class", "state-line")
+                     // .attr("state_id",(data) => (data.cve_edo))
+                     .append("path")
+                     .attr("class", "state-line")
+                     .attr("d", d => line((d)))
+                     // .attr("d",function (geodata,data,d){ if (data.cve_edo===geodata.CVE_ENT){
+                     //   line((d))
+                     // }})
+                     .attr("stroke", '#2171b5')
+                     //.attr("stroke", (d,i) => i % 2 ? 'red' : '#2171b5')
 
+                     .attr("fill", "none");
+                     // .on(“mouseout”, function(d) {
+                     //     svg.select(“.title-text”).remove();
+                     //   })
+              console.log(this)
+              // svg_chart.append("path")
+              // .datum(data) // Binds data to the line
+              // .attr("class", "line") // Assign a class for styling
+              // .attr("d", line(data))
+              // .attr("fill", "none")// Calls the line generator
+              // .attr("stroke", '#2171b5');
+            })
+
+    // done: function(data) {
+    //           data.svg_map.selectAll('.datamaps-subunit').on('click', function(geography) {
+    //               var m = {};
+    //               m[geography.id] = '#000000';
+    //               datamap.updateChoropleth(m);
+    //           });
+    //         }
+            /* Add line into SVG */
+     // var line = d3.line()
+     //   .x(d => xScale(d.Time))
+     //   .y(d => yScale(d.FLFP));
+     //
+     // let lines = svg.append(‘g’)
+     //   .attr(‘class’, ‘lines’);
+     //
+     // lines.selectAll(‘.line-group’)
+     //   .data(data).enter()
+     //   .append(‘g’)
+     //   .attr(‘class’, ‘line-group’)
+     //   .on(“mouseover”, function(d, i) {
+     //       svg.append(“text”)
+     //         .attr(“class”, “title-text”)
+     //         .style(“fill”, Regcolor(i))
+     //         .text(d.name)
+     //         .attr(“text-anchor”, “middle”)
+     //         .attr(“x”, (width-margin)/2)
+     //         .attr(“y”, 5);
+     //     })
+     //   .on(“mouseout”, function(d) {
+     //       svg.select(“.title-text”).remove();
+     //     })
+     //   .append(‘path’)
+     //   .attr(‘class’, ‘line’)
+     //   .attr(‘d’, d => line(d.values))
+     //   .style(‘stroke’, (d, i) => Regcolor(i))
+     //   .style(‘opacity’, lineOpacity)
+     //   .on(“mouseover”, function(d) {
+     //       d3.selectAll(‘.line’)
+     //           .style(‘opacity’, otherLinesOpacityHover);
+     //       d3.selectAll(‘.circle’)
+     //           .style(‘opacity’, circleOpacityOnLineHover);
+     //       d3.select(this)
+     //         .style(‘opacity’, lineOpacityHover)
+     //         .style(“stroke-width”, lineStrokeHover)
+     //         .style(“cursor”, “pointer”);
+     //     })
+     //   .on(“mouseout”, function(d) {
+     //       d3.selectAll(“.line”)
+     //           .style(‘opacity’, lineOpacity);
+     //       d3.select(this)
+     //         .style(“stroke-width”, lineStroke)
+     //         .style(“cursor”, “none”);
+     //     });
+
+
+
+
+            // .on("click", function(){
+            //   d3.select(this).
+            //   style("fill", "#2171b5");});
+  // svg_map.append("g")
+  //     .attr("class", "x axis")
+  //     //.attr("transform", "translate(0," + height + ")")
+  //     .attr("transform", "translate(200, 0)")
+  //     .attr("class", "nothing")
+  //     //.attr("transform", "translate(0," + 100  + ")")
+  //     .attr("transform", "translate(49,0)")
+  //     .call(yAxis);
+
+    //  .call(xAxis);
+
+
+//
   // svg.selectAll(".dot")
   //     .data(data)
   //   .enter().append("circle") // Uses the enter().append() method
