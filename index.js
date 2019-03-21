ac35f33c169a910bc5e86e20972c9c379b481ae4
Aleister Montfort <aleistermontfort@Aleisters-MacBook-Pro.local>
2019-03-21 18:17:59 -0500

    change plot height

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ for (i = 0; i < 32; i++) {
   const geoGenerator = d3.geoPath(projection);
   console.log(geoGenerator.centroid(geodata))
   projection.fitExtent([[0, 0], [650, 500]], geodata);
-  projection.fitSize([750,400],geodata);
+  projection.fitSize([700,400],geodata);
 
 
   // X SCALE, years
